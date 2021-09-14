c594f002c4295c701a5d82197603ad73381a49fd
Akil Juhary <akiljuhary1@gmail.com>
2021-09-14 19:18:05 +0530

    Auto age

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,9 @@
-const displayAge = document.getElementsByClassName('dob');
+const displayAge = document.getElementsByClassName("dob");
+const autoAge = document.getElementsByClassName("autoAge");
+
+function showAge() {
+    const currentAge = dob - new Date().getFullYear();
+    document.autoAge.currentAge;
+};
+
+console.log(showAge());
